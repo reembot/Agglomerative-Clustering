1a88a278716de96a7fe0d21740b8495bb01f817d
Reema Shadid <93971270+reembot@users.noreply.github.com>
2022-10-30 22:59:55 -0700

    Update agglo_avg_time_scripts.sh

--- agglo_avg_time_scripts.sh
+++ agglo_avg_time_scripts.sh
@@ -25,12 +25,28 @@ do
     ((count++))
 done
 echo successfully executed $count times, output written to avg100p_output.txt
-echo Testing dataset size of 500 points 5 times:
+echo Testing dataset size of 120 points 5 times:
 count=0
 while [ $count -le 4 ]
 do
-    python3 hierarchicalclustering.py time 500 avg >>avg500p_output.txt; sync
+    python3 hierarchicalclustering.py time 120 avg >>avg120p_output.txt; sync
     ((count++))
 done
-echo successfully executed $count times, output written to avg500p_output.txt
+echo successfully executed $count times, output written to avg120p_output.txt
+echo Testing dataset size of 150 points 5 times:
+count=0
+while [ $count -le 4 ]
+do
+    python3 hierarchicalclustering.py time 150 avg >>avg150p_output.txt; sync
+    ((count++))
+done
+echo successfully executed $count times, output written to avg150p_output.txt
+echo Testing dataset size of 200 points 5 times. This might take a few minutes:
+count=0
+while [ $count -le 4 ]
+do
+    python3 hierarchicalclustering.py time 200 avg >>avg200p_output.txt; sync
+    ((count++))
+done
+echo successfully executed $count times, output written to avg200p_output.txt
 echo All Done!
